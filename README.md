8172f18cafe056d8c4554666332d6cef6448c728
Ramands07 <dsraman07@gmail.com>
2026-08-23 10:58:54 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## END to END REALTIME FRAUD DETECTION 
+
+Steps followed in creating this project
+
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+from typing import List
+
+def get_rquirements(file_path:str)->list(str):
+    '''Will import all the library from here'''
+    
+    requirements =[]
+    
+    with open file_path as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.strip() for req in requirements]
+        
+    return requirements
+
+
+setup(
+    name = 'Realtime_frad_detection',
+    version = '0.0.1',
+    author='Ramands7',
+    author_email='dsraman07@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+    
+)
+        
+     
